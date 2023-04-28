76faafb1951d61faef5a3648bc204bcf6890eed6
Albert Dupre <aldu@localhost.localdomain>
2023-04-28 15:15:47 +0200

    spinner + angularjs impl

---
+++ angular-spinner.js
@@ -0,0 +1,16 @@
+var spinnerApp = angular.module('spinnerMod', []);
+
+spinnerApp.controller('spinnerCtrl', ['$scope', '$interval',  function($scope, $interval){
+	$scope.running= false;
+
+  const nb_seconds = 5;
+  $scope.seconds = nb_seconds;
+  $scope.run = function() {
+		if(! $scope.running){
+			$scope.running = true;
+			$interval(function(){ $scope.running = false; $scope.seconds = nb_seconds }, nb_seconds * 1000, 1);
+			$interval(function(){ $scope.seconds -= 1; }, 1000, $scope.seconds-1);
+
+		}
+	}	
+}]);
